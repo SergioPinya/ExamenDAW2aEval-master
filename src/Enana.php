f30e066cf34b2e5e8f4c4df0fb7d7592351977fe
Sergio <msegio2003@gmail.com>
2023-02-07 13:45:34 +0100

    heridav1

--- src/Enana.php
+++ src/Enana.php
@@ -18,11 +18,14 @@ class Enana
         #Se le quitan 10 puntos de vida a la Enana y además se cambia el valor de situacion (si fuera necesario)
      
         if ($this->puntosVidao>0) {
-        return $this->situacion = 'muerta';
+        $this->situacion = 'muerta';
+        return $this->situacion;
         }elseif ($this->puntosVida=0) {
-            return $this->situacion = 'limbo';
+            $this->situacion = 'limbo';
+            return $this->situacion;
         }else {
-            return $this->puntosVida-10;
+            $this->puntosVida-10;
+            return $this->puntosVida;
         }
 
     }
